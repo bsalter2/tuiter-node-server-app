e2da5f281c7f28e72edbc526dbc85785bfab5091
bsalter2 <brendansalter97@gmail.com>
2023-06-05 23:33:13 -0700

    added allowed origin

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ app.use(
 app.use(
     cors({
         credentials: true,
-        origin: "http://localhost:3000",
-    })
+        origin: ["http://localhost:3000", "https://a5--stalwart-phoenix-74ac4b.netlify.app"]
+        })
 );
 
 app.use(express.json())
