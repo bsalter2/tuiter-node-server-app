d6eab49cb31661b7ef35d15805c62161fe6ff440
bsalter2 <brendansalter97@gmail.com>
2023-06-05 19:17:57 -0700

    first commit

---
+++ users/users.js
@@ -0,0 +1,5 @@
+export default [
+    { "username": "test", 'password': "test",   "type": "FACULTY", "_id": "123" },
+    { "username": "bob",  'password': "test",   "type": "STUDENT", "_id": "234" },
+    { "username": "charlie", 'password': "test", "type": "FACULTY", "_id": "345" }
+]  
