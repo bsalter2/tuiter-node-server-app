a52df9a6a97470d2fdb62c4d2b0ceac05ba4c653
bsalter2 <brendansalter97@gmail.com>
2023-06-13 23:57:55 -0700

    reverted main for assignment a5

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-import mongoose from "mongoose";
-mongoose.connect("mongodb+srv://admin_test:password_test@cluster92221.4bbnaba.mongodb.net/tuiter?retryWrites=true&w=majority").then(
-    () => { 
-       console.log("Connected to DB!");
-   },
-    err => { 
-      console.log(err);
-   }
-  );
+import express from 'express'
+import session from "express-session";
+import cors from 'cors'
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./users/auth-controller.js";
+
+const app = express()
+app.use(
+    session({
+        secret: "any string",
+        resave: false,
+        saveUninitialized: true,
+    })
+);
+app.use(
+    cors({
+        credentials: true,
+        origin: "http://localhost:3000",
+    })
+);
+
+app.use(express.json())
+HelloController(app)
+UserController(app)
+TuitsController(app);
+AuthController(app);
+
+const port = process.env.PORT || 4000;
+app.listen(port)
