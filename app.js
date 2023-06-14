1f354ebf6ff4f9ad4c15b96ab7b01d2dd4423684
bsalter2 <brendansalter97@gmail.com>
2023-06-13 17:19:43 -0700

    quick fixes

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
-import {MongoClient, ServerApiVersion } from 'mongodb';
-const uri = "mongodb+srv://admin_test:password_test@cluster92221.4bbnaba.mongodb.net/tuiter?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+import mongoose from "mongoose";
+mongoose.connect("mongodb+srv://admin_test:password_test@cluster92221.4bbnaba.mongodb.net/tuiter?retryWrites=true&w=majority");
