697d6d1e366099a33a475ba94c6933b1e59d0fe0
bsalter2 <brendansalter97@gmail.com>
2023-06-13 23:43:29 -0700

    a6 su23

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 import mongoose from "mongoose";
-mongoose.connect("mongodb+srv://admin_test:password_test@cluster92221.4bbnaba.mongodb.net/tuiter?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://admin_test:password_test@cluster92221.4bbnaba.mongodb.net/tuiter?retryWrites=true&w=majority").then(
+    () => { 
+       console.log("Connected to DB!");
+   },
+    err => { 
+      console.log(err);
+   }
+  );
