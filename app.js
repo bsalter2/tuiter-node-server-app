dc72c25ae19caf8805f1a5813e7698fd394ebb00
bsalter2 <brendansalter97@gmail.com>
2023-06-14 00:05:38 -0700

    a6 su23

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import AuthController from "./users/auth-controller.js";
+import mongoose from "mongoose";
 
 const app = express()
 app.use(
@@ -29,3 +30,13 @@ AuthController(app);
 
 const port = process.env.PORT || 4000;
 app.listen(port)
+
+mongoose.connect("mongodb+srv://admin_test:password_test@cluster92221.4bbnaba.mongodb.net/tuiter?retryWrites=true&w=majority").then(
+    () => { 
+       console.log("Connected to DB!");
+   },
+    err => { 
+      console.log(err);
+   }
+  );
+
